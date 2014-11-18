ca8706ec33bbdded608a98b42a5250c3c4c91c2a
Cryptkeeper <cryptkeeper@hypixel.net>
2014-11-18 07:25:36 -0600

    Session API and RDoc fixes.

--- lib/hypixel-ruby.rb
+++ lib/hypixel-ruby.rb
@@ -2,6 +2,7 @@ require_relative 'hypixel-ruby/api'
 require_relative 'hypixel-ruby/guild'
 require_relative 'hypixel-ruby/friend'
 require_relative 'hypixel-ruby/player'
+require_relative 'hypixel-ruby/session'
 
 require_relative 'hypixel-ruby/statholder/generic'
 require_relative 'hypixel-ruby/statholder/blitz'
