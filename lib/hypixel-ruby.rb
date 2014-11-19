5da0520a5c624b9d6d7da61ac25fc79ada942843
Cryptkeeper <cryptkeeper.dev@gmail.com>
2014-11-18 21:28:01 -0600

    String to GameType symbol support.

--- lib/hypixel-ruby.rb
+++ lib/hypixel-ruby.rb
@@ -3,6 +3,7 @@ require_relative 'hypixel-ruby/guild'
 require_relative 'hypixel-ruby/friend'
 require_relative 'hypixel-ruby/player'
 require_relative 'hypixel-ruby/session'
+require_relative 'hypixel-ruby/gametype'
 
 require_relative 'hypixel-ruby/statholder/generic'
 require_relative 'hypixel-ruby/statholder/blitz'
