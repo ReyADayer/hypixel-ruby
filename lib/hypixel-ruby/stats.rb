a538b2b08d7b77eff3151cf952849fe6a96c799f
Cryptkeeper <cryptkeeper@hypixel.net>
2014-11-20 18:06:14 -0600

    Drop StatHolder system for new Stats class.

--- lib/hypixel-ruby/stats.rb
+++ lib/hypixel-ruby/stats.rb
@@ -2,14 +2,33 @@ module Hypixel
 
     class Stats
 
+        attr_reader :packages, :coins
+
+        # Constructs a Stats instance using the JSON object.
+        # Wraps the initialize method for future changes.
+        #
+        # Params:
+        # +json+::The JSON object to construct from.
         def self.from_json(json)
-            Stats.new json
+            Stats.new (json['stats'] ||= {})
         end
 
         private
 
+        # Offloads the JSON object's values to the local instance.
+        #
+        # Params:
+        # +json+::The JSON object to construct from.
         def initialize(json)
+            @packages = {}
+            @coins = {}
 
+            json.keys.each do | key |
+                gameType = GameType.from_db_name key
+
+                @packages[gameType] = json[key]['packages'] ||= []
+                @coins[gameType] = json[key]['coins'] ||= 0
+            end
         end
     end
 end
